8979a9883555f4eccfa1e5e1e478efea9d14e49d
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-17 12:03:15 -0600

    add search methods in PersonaRepository and update SpringbootJpaApplication to display results

--- src/main/java/com/zutjmx/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/zutjmx/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -25,16 +25,32 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		System.out.println("Ejecutando el método run de la interfaz CommandLineRunner");
 		List<Persona> personas = (List<Persona>) personaRepository.findAll();
 		
-		System.out.println("Listado de personas usando un for");
-		for (Persona persona : personas) {
-			System.out.println(persona);
-		}
-
-		System.out.println("Listado de personas usando un forEach con referencia a un método");
-		personas.stream().forEach(System.out::println);
-
 		System.out.println("Listado de personas usando un forEach con expresión lambda");
 		personas.stream().forEach(persona -> System.out.println(persona));
+
+		System.out.println("Busqueda de personas por lenguaje de programación");
+		List<Persona> personasRank = personaRepository.findByLenguajeProgramacion("Rank");
+		personasRank.stream().forEach(persona -> System.out.println(persona));
+
+		System.out.println("Busqueda de personas por nombre");
+		List<Persona> personasNombre = personaRepository.findByNombre("Papagena");
+		personasNombre.stream().forEach(persona -> System.out.println(persona));
+
+		System.out.println("Busqueda de personas por email");
+		List<Persona> personasEmail = personaRepository.findByEmail("aitzakovitz10@goodreads.com");
+		personasEmail.stream().forEach(persona -> System.out.println(persona));
+
+		System.out.println("Busqueda de personas por apellido paterno");
+		List<Persona> personasPaterno = personaRepository.buscarPorPaterno("Jeafferson");
+		personasPaterno.stream().forEach(persona -> System.out.println(persona));
+
+		System.out.println("Busqueda de personas por apellidos paterno y materno");
+		List<Persona> personasPaternoMaterno = personaRepository.buscarPorPaternoYMaterno(
+			"Hannant", 
+			"Godart"
+		);
+		personasPaternoMaterno.stream().forEach(persona -> System.out.println(persona));
+
 	}
 
 }
--- src/main/java/com/zutjmx/curso/springboot/jpa/springbootjpa/repositories/PersonaRepository.java
+++ src/main/java/com/zutjmx/curso/springboot/jpa/springbootjpa/repositories/PersonaRepository.java
@@ -1,9 +1,21 @@
 package com.zutjmx.curso.springboot.jpa.springbootjpa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.zutjmx.curso.springboot.jpa.springbootjpa.entities.Persona;
 
 public interface PersonaRepository extends CrudRepository<Persona, Long> {
 
+    List<Persona> findByNombre(String nombre);
+    List<Persona> findByEmail(String email);
+    List<Persona> findByLenguajeProgramacion(String lenguajeProgramacion);
+
+    @Query("SELECT p FROM Persona p WHERE p.paterno = ?1")
+    List<Persona> buscarPorPaterno(String paterno);
+
+    @Query("SELECT p FROM Persona p WHERE p.paterno = ?1 and p.materno = ?2")
+    List<Persona> buscarPorPaternoYMaterno(String paterno, String materno);
 }
